8d600e8c72efa43c0189d09ffa3e183d8537d528
Jamie T <jethompson81@gmail.com>
2018-02-19 20:13:04 -0600

    Random Changes

--- home_wx_displayMQTT.py
+++ home_wx_displayMQTT.py
@@ -43,25 +43,29 @@ from math import isnan
 import paho.mqtt.client as mqtt
 import time
 import json
+import logging
 
 dht_sensor_port = 7 # connect the DHt sensor to port 7
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
 local_time = int(time.time()) # grabbing timestamp and converting to int
-mosquitto_org_topic = "SNHU/IT697/james_thompson_snhu_edu/sensor/data/json"
-test_mosquitto_host = "test.mosquitto.org"
-local_mqtt_host = "localhost"
+mosquitto_org_topic = "SNHU/IT697/james_thompson_snhu_edu/sensor/data/json" #MQTT Topic
+test_mosquitto_host = "test.mosquitto.org" #Remote test broker
+local_mqtt_host = "localhost" # No place like home
 
 # set green as backlight color
 # we need to do it just once
-setRGB(0,0,0)
+setRGB(0,255,0)
 # Variables for temperature thresholds.
-WARM = 74.0
-COOL = 73.0
+warmF = 74.0
+justRightF = 72.0
+coolF = 73.0
+
 # Function to convert Celsius to Fahrenheit
 def celToFahr(temp):
         tempf = float((temp * 9 / 5 ) + 32 )
         return tempf
 
+# Function to build the json data to publish
 def buildJson(tempf, hum):
     data = {
         'timestamp': local_time,
@@ -73,10 +77,10 @@ def buildJson(tempf, hum):
     data_json = json.dumps(data)
     return data_json
 
+# Function to connect publish to the MQTT broker (needs the hostname, temp and humidity inputs)
 def publishMQTT(host, tempf, hum):
     local_client = mqtt.Client()
     local_client.connect(host)
-    # local_client.publish(mosquitto_org_topic, "Temp: " + t + " Humidity: " + h)
     local_client.publish(mosquitto_org_topic, buildJson(tempf, hum))
 
 while True:
@@ -86,18 +90,28 @@ while True:
 
                 tempf = celToFahr(temp)
 
-
+                # Change the screen colors
+                if tempf >= warmF:
+                    logging.warning('I am warm')
+                    setRGB(255, 0, 0)
+                elif tempf <= coolF:
+                    logging.warning("I am cool")
+                    setRGB(0, 0, 255)
+                elif tempf == justRightF:
+                    logging.warning('I am just right')
+                    setRGB(0, 255, 0)
                 # check if we have nans
                 # if so, then raise a type error exception
                 if isnan(temp) is True or isnan(hum) is True:
                         raise TypeError('nan error')
 
-                print("temp =", tempf, "C\thumidity =", hum,"%")
+                print("temp =", tempf, "F\thumidity =", hum,"%")
 
 
 
                 t = str(tempf)
                 h = str(hum)
+
                 # MQTT Publish to remote
                 publishMQTT(test_mosquitto_host,tempf,hum)
                 # MQTT Publish to local
@@ -108,10 +122,7 @@ while True:
         # instead of inserting a bunch of whitespace, we can just insert a \n
         # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
                 setText_norefresh("Temp:" + t + "F\n" + "Humidity :" + h + "%")
-                if tempf >= WARM:
-                    setRGB(255,0,0)
-                elif tempf <= COOL:
-                    setRGB(0,0,255)
+
                 sleep(1)
 
         except (IOError, TypeError) as e:
